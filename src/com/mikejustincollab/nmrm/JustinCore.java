a9b407ca66d64e6fa0b9f917dd7537f8eb11eab1
codewalker1337 <jrgolden2011@live.com>
2014-07-01 00:25:22 -0400

    Added Shit and chicken bits

--- src/com/mikejustincollab/nmrm/JustinCore.java
+++ src/com/mikejustincollab/nmrm/JustinCore.java
@@ -1,5 +1,7 @@
 package com.mikejustincollab.nmrm;
 
+import com.mikejustincollab.nmrm.blocks.JBlockRecipes;
+import com.mikejustincollab.nmrm.blocks.JBlocks;
 import com.mikejustincollab.nmrm.items.JItemRecipes;
 import com.mikejustincollab.nmrm.items.JItems;
 
@@ -15,17 +17,28 @@ public class JustinCore {
 	 * Gets called when forge initializes this mod.
 	 */
 	public static JItems jItems;
+	public static JBlocks jBlocks;
+	public static JBlockRecipes jBlockRecipes;
 	public static JCreativeTab jtab;
 	public static JItemRecipes jItemRecipes;
+	
+	
+	
 	public JustinCore() {
 		init();
 	}
 	
 	private void init() {
 		jItems = new JItems();
+		jBlocks = new JBlocks();
 		jItemRecipes = new JItemRecipes();
+		jBlockRecipes = new JBlockRecipes();
+		
 		jtab = new JCreativeTab("NmrmFood", jItems.moleRatFlesh);
+		
 		jItems.setCreativeTabs();
+		
+		
 		jItemRecipes.loadRecipes();
 		
 		
